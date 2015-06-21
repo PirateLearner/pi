9ebb2d04bf2ed604c6427a332d4442642e44eda0
Anshul <anshulthakurjourneyendless@gmail.com>
2015-06-21 09:51:26 +0530

    Removed include folder. Updated Settings.py

--- PirateLearner/PirateLearner/settings.py
+++ PirateLearner/PirateLearner/settings.py
@@ -320,19 +320,86 @@ THUMBNAIL_ALIASES = {
 }
 CKEDITOR_UPLOAD_PATH = 'images/'
 CKEDITOR_CONFIGS = {
+    'toolbar_Custom':[
+                      ["Format", "Bold", "Italic", "Underline", "Strike", "SpellChecker"],
+                      ['NumberedList', 'BulletedList', "Indent", "Outdent", 'JustifyLeft', 'JustifyCenter',
+                 'JustifyRight', 'JustifyBlock'],
+                      ["Image", "Table", "Link", "Unlink", "Anchor", "SectionLink", "Subscript", "Superscript"], 
+                      ['Undo', 'Redo'], ["Source"],["Maximize"],['About','Code'],
+                     ],
+    
     'default': {
-        'toolbar': 'Full',
-        'justifyClasses': [ 'AlignLeft', 'AlignCenter', 'AlignRight', 'AlignJustify' ],
+        'toolbar': [
+                      ["Format", "Bold", "Italic", "Underline", "Strike", "SpellChecker"],
+                      ['NumberedList', 'BulletedList', "Indent", "Outdent", 'JustifyLeft', 'JustifyCenter',
+                 'JustifyRight', 'JustifyBlock'],
+                      ["Image", "Table", "Link", "Unlink", "Anchor", "SectionLink", "Subscript", "Superscript"], 
+                      ['Undo', 'Redo'], ["Source"],["Maximize"],['About','Code'],
+                     ],
+        'contentsCss': STATIC_URL+'css/bootstrap.css',
+    
+        'codemirror' : {
+                        # Set this to the theme you wish to use (codemirror themes)
+                        'theme': 'default',
+                        # Whether or not you want to show line numbers
+                        'lineNumbers': 'true',
+                        # Whether or not you want to use line wrapping
+                        'lineWrapping': 'true',
+                        # Whether or not you want to highlight matching braces
+                        'matchBrackets': 'true',
+                        # Whether or not you want tags to automatically close themselves
+                        'autoCloseTags': 'true',
+                        # Whether or not you want Brackets to automatically close themselves
+                        'autoCloseBrackets': 'true',
+                        # Whether or not to enable search tools, CTRL+F (Find), CTRL+SHIFT+F (Replace), CTRL+SHIFT+R (Replace All), CTRL+G (Find Next), CTRL+SHIFT+G (Find Previous)
+                        'enableSearchTools': 'true',
+                        # Whether or not you wish to enable code folding (requires 'lineNumbers' to be set to 'true')
+                        'enableCodeFolding': 'true',
+                        # Whether or not to enable code formatting
+                        'enableCodeFormatting': 'true',
+                        # Whether or not to automatically format code should be done when the editor is loaded
+                        'autoFormatOnStart': 'true',
+                        # Whether or not to automatically format code should be done every time the source view is opened
+                        'autoFormatOnModeChange': 'true',
+                        # Whether or not to automatically format code which has just been uncommented
+                        'autoFormatOnUncomment': 'true',
+                        # Define the language specific mode 'htmlmixed' for html including (css, xml, javascript), 'application/x-httpd-php' for php mode including html, or 'text/javascript' for using java script only
+                        'mode': 'htmlmixed',
+                        # Whether or not to show the search Code button on the toolbar
+                        'showSearchButton': 'true',
+                        # Whether or not to show Trailing Spaces
+                        'showTrailingSpace': 'true',
+                        # Whether or not to highlight all matches of current word/selection
+                        'highlightMatches': 'true',
+                        # Whether or not to show the format button on the toolbar
+                        'showFormatButton': 'true',
+                        # Whether or not to show the comment button on the toolbar
+                        'showCommentButton': 'true',
+                        # Whether or not to show the uncomment button on the toolbar
+                        'showUncommentButton': 'true',
+                        #Whether or not to show the showAutoCompleteButton button on the toolbar
+                        'showAutoCompleteButton': 'true',
+                        # Whether or not to highlight the currently active line
+                        'styleActiveLine': 'true'
+                        },
+        
     },
 }
-
-CRISPY_TEMPLATE_PACK = 'bootstrap3'
+CKEDITOR_RESTRICT_BY_USER=True
 
 DISQUS_API_KEY = 'QJezRiHWxv2FzzrMuOSvQPn99oil0LLyhZxdCAEd3s5cZTf6GUI5019NKznCEONu'
 DISQUS_WEBSITE_SHORTNAME = 'piratelocal'
 
+DEFAULT_FROM_EMAIL = 'rai812@web379.webfaction.com'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
 EMAIL_SUBJECT_PREFIX = '[PirateLearner]'
+EMAIL_HOST = 'smtp.webfaction.com'
+EMAIL_HOST_USER = 'pirate_learner_mailbox'
+EMAIL_HOST_PASSWORD = 'pirate@world'
+SERVER_EMAIL = 'rai812@web379.webfaction.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 25
 
 
 MATHJAX_ENABLED=True
